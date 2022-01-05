7188aad42bd115f39636e0ca75eded5f48e74a63
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-05 21:59:50 +0700

    update

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,11 +3,23 @@ import { useLocation } from 'react-router-dom'
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+let socket;
+
 const Chat = () => {
+   const [name, setName] = useState('');
+   const [room, setRoom] = useState('');
    const location = useLocation();
+   const ENDPOINT = 'localhost:3000';
    useEffect(() => {
-      const data = queryString.parse(location.search);
-      console.log(data);
+      console.log(location.search);
+      const { name, room } = queryString.parse(location.search);
+
+      socket = io(ENDPOINT);
+
+      console.log(name, room);
+      console.log(socket);
+      setName(name);
+      setRoom(room);
    })
    return (
       <h1>Chat</h1>

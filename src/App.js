2581bfed79854d6577e83fcb164f2a3ec74f90cb
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-05 20:59:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Join/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Join/Join.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Join.css';
+
+const Join = () => {
+   const [name, setName] = useState('');
+   const [room, setRoom] = useState('')
+
+   return (
+     <>
+       <div className="joinOuterContainer">
+         <div className="joinInnerContainer container center">
+           <h2 className="header">join chat room</h2>
+           <div>
+             <input
+               type="text"
+               name="userName"
+               placeholder="username"
+               value={name}
+               onChange={(e) => {
+                  setName(e.target.value);
+               }}
+             />
+           </div>
+           <div>
+             <input
+               type="text"
+               name="roomName"
+               placeholder="room name"
+               value={room}
+               onChange={(e) => {
+                  setRoom(e.target.value);
+               }}
+             />
+           </div>
+           <Link onClick={
+              (e) => (!name || !room) ? e.preventDefault() : null
+           } to={`/chat?name=${name}&room=${room}`}>
+               <button className="btn" type="submit" style={{marginTop: '15px'}}>sign in</button>
+           </Link>
+         </div>
+       </div>
+     </>
+   );
+}
+
+export default Join

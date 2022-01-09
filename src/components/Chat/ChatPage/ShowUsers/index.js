42bee72c1e4d4bd2a11e248ccc776cbe6e48d222
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-09 23:11:56 +0700

    update

--- src/components/Chat/ChatPage/ShowUsers/index.js
+++ src/components/Chat/ChatPage/ShowUsers/index.js
@@ -3,17 +3,25 @@ import React from 'react'
 import './index.css'
 
 const index = ({ users, removePopUp }) => {
-   return (
-     <div className="usersBox">
-       <h2 className="usersPopUpHeader">Members</h2>
-       {users.map((user) => (
-         <p key={user.id} className="userList">
-           {user.name}
-         </p>
-       ))}
-      <i onClick={removePopUp} class="fas fa-window-close red" id="closePopUpButton"></i>
-     </div>
-   );
+  return (
+    <div className="usersBox">
+      <div className="innerUsersBox">
+        <h2 className="usersPopUpHeader">Members</h2>
+        <div className="usersListScrollArea">
+          {users.map((user) => (
+            <p key={user.id} className="userList">
+              {user.name}
+            </p>
+          ))}
+        </div>
+        <i
+          onClick={removePopUp}
+          className="fas fa-window-close red"
+          id="closePopUpButton"
+        ></i>
+      </div>
+    </div>
+  );
 }
 
 export default index
